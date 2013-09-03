9980128af88b4f8a6e9d6ffc2532d6522e10e18d
vitrum.zhu <vitrum.cn@gmail.com>
2013-09-04 06:58:10 +0800

    update

--- www/js/mt.js
+++ www/js/mt.js
@@ -29,7 +29,7 @@ var AppRouter = Backbone.Router.extend({
       fntA.gameFinish =  false;
       fntA.shakerecord = 0;
       fntA.shakeEng = 0;
-      $('.playrecord').html();
+      $('.playrecord').html('0米');
       router.navigate('run');
     }
   	showSubFrame('runbox','qrcodebox');
@@ -161,18 +161,24 @@ function postLogin(){
       console.log(json);
       //var jsdata = eval('('+json+')');  
       var jsdata = json;
-      console.log('status='+ jsdata.status);
+      console.log(jsdata);
       fntA.playerName = jsdata.user_name;
       fntA.playerId = jsdata.user_id;
       fntA.playerAvatar = 'http://tnf-avatar.b0.upaiyun.com/'+jsdata.user_avatar;
       router.navigate('run');
       showSubFrame('runbox','qrcodebox');
+      $('.playerinfoa .playername').html(fntA.playerName);
+      if(jsdata.user_avatar!==''){
+        $('.playerinfoa img').attr('src',fntA.playerAvatar);
+      }
+      
       //console.log('mid='+ jsdata.data.mid );
     },
     error: function(xhr, type){
       console.log('Ajax error!')
     }
   });
+  fntRun();
 }
 function postRegister(){
   var userName  = $('#reguname').val()
@@ -223,7 +229,8 @@ function funMapload(){
 		'img/map/map_c_03.jpg',
 		'img/map/map_a_b.jpg' ,
 		'img/map/map_b_c.jpg' ,
-		'img/map/map_c_a.jpg' ];
+		'img/map/map_c_a.jpg' ,
+    'img/player-mini.png'];
   fntA.pcplayerName = [
     '何川',
     '李北基',
@@ -251,6 +258,7 @@ function fntRun(){
   fntA.image3 = new Image();  
   fntA.image4 = new Image();  
   fntA.image5 = new Image();
+  fntA.imageMini = new Image();
   fntA.w = 320;  
   fntA.h = 490; 
   var ctx0,ctx1;  
@@ -304,6 +312,7 @@ function fntRun(){
   function runInit() {
     ctx0 =  document.getElementById('canvas').getContext('2d');
     ctx1 =  document.getElementById('canvas2').getContext('2d');
+    ctxMini =  document.getElementById('minimap').getContext('2d');
     fntA.mapArr = new Array();
     y0 = 0;
     y1 = -1*fntA.h;
@@ -345,10 +354,12 @@ function fntRun(){
       fntA.image3.src = fntA.mapArr[0];  
       fntA.image4.src = fntA.mapArr[1];
       fntA.image5.src = fntA.mapArr[2];
+      fntA.imageMini.src = fntA.imgArr[12];
     var i = fntA.pcplayerName.length;
     var t = fRandomBy(0,i);
     $('.playerinfob .playername').html('TNF运动员  '+fntA.pcplayerName[t]);
     $('.playerinfob img').attr('src','img/player/' + fntA.pcplayerName[t] + '.jpg');
+
 
     setTimeout(function () {
       ctx0.drawImage(fntA.image0,0,y0,fntA.w,fntA.h);  
@@ -357,8 +368,10 @@ function fntRun(){
       ctx1.drawImage(fntA.image3,0,y3,fntA.w,fntA.h);  
       ctx1.drawImage(fntA.image4,0,y4,fntA.w,fntA.h);  
       ctx1.drawImage(fntA.image5,0,y5,fntA.w,fntA.h); 
+      ctxMini.drawImage( fntA.imageMini,9,430,29,28);
+      ctxMini.drawImage( fntA.imageMini,59,430,29,28);
       console.log('draw default map image');
-    }, 100);
+    }, 40);
 
 
   }
@@ -388,8 +401,10 @@ function fntRun(){
       if (fntA.requestId)
         window.cancelAFrame(fntA.requestId); 
       $('.player').removeClass('running'); 
-      $('.gameover').html('游戏结束！fntA.allmoveA :'+ fntA.allmoveA + ',fntA.allmoveB:' +fntA.allmoveB);
-      $('.gameover').show();      
+      //clear
+      //ctx0.clearRect(0,0,fntA.w,fntA.h);
+      //ctx1.clearRect(0,0,fntA.w,fntA.h); 
+      ctxMini.clearRect(0,0,fntA.w,fntA.h);     
     }
     function wayRoll(e) {
       if((e+1) > fntA.mapitem ){
@@ -400,7 +415,8 @@ function fntRun(){
     function render(time) {
       //clear
       ctx0.clearRect(0,0,fntA.w,fntA.h);
-      ctx1.clearRect(0,0,fntA.w,fntA.h);  
+      ctx1.clearRect(0,0,fntA.w,fntA.h); 
+      ctxMini.clearRect(0,0,fntA.w,fntA.h);
       //draw now
       var move = Math.floor(fntA.moveA);
       var moveB = Math.floor(fntA.moveB);
@@ -410,19 +426,20 @@ function fntRun(){
       y2 +=move; 
       //console.log("new: y0=" + y0 + ",y1=" + y1 + ",y2=" + y2 + ",move=" + move + ",fntA.moveA=" + fntA.moveA); 
       if(y0>=fntA.h){  
-        y0=move-2*fntA.h;  
+        //y0=move-2*fntA.h;  
+        y0 = Math.min(y1,y2) - fntA.h;
         fntA.alltimes++;
         fntA.image0.src = fntA.mapArr[wayRoll((Number(fntA.alltimes)+3))]; 
         console.log("y0 new image:" + fntA.image0.src + "fntA.alltimes:"+fntA.alltimes);
       }  
       if(y1>=fntA.h){  
-        y1=move-2*fntA.h;  
+        y1 = Math.min(y0,y2) - fntA.h;
         fntA.alltimes++;
         fntA.image1.src = fntA.mapArr[wayRoll((Number(fntA.alltimes)+3))];
         console.log("y1 new image:" + fntA.image1.src+ "fntA.alltimes:"+fntA.alltimes);
       }  
       if(y2>=fntA.h){  
-        y2=move-2*fntA.h; 
+        y2 = Math.min(y0,y1) - fntA.h;
         fntA.alltimes++; 
         fntA.image2.src = fntA.mapArr[wayRoll((Number(fntA.alltimes)+3))];
         console.log("y2 new image:" + fntA.image2.src+ "fntA.alltimes:"+fntA.alltimes);
@@ -439,19 +456,19 @@ function fntRun(){
       
       //console.log("new: y3=" + y3 + ",y4=" + y4 + ",y5=" + y5 + ",moveB=" + moveB + ",fntA.moveB=" + fntA.moveB); 
       if(y3>=fntA.h){  
-        y3=moveB-2*fntA.h;  
+        y3 = Math.min(y4,y5) - fntA.h; 
         fntA.alltimesB++;
         fntA.image3.src = fntA.mapArr[wayRoll((Number(fntA.alltimesB)+3))]; 
         console.log("y3 new image:" + fntA.image3.src+ "fntA.alltimesB:"+fntA.alltimesB + ",image id:" + (Number(fntA.alltimesB)+3));
       }  
       if(y4>=fntA.h){  
-        y4=moveB-2*fntA.h;  
+        y4 = Math.min(y3,y5) - fntA.h; 
         fntA.alltimesB++;
         fntA.image4.src = fntA.mapArr[wayRoll((Number(fntA.alltimesB)+3))];
         console.log("y4 new image:" + fntA.image4.src+ "fntA.alltimesB:"+fntA.alltimesB+ ",image id:" + (Number(fntA.alltimesB)+3));
       }  
       if(y5>=fntA.h){  
-        y5=moveB-2*fntA.h; 
+        y5 = Math.min(y3,y4) - fntA.h;
         fntA.alltimesB++; 
         fntA.image5.src = fntA.mapArr[wayRoll((Number(fntA.alltimesB)+3))];
         console.log("y5 new image:" + fntA.image5.src+ "fntA.alltimesB:"+fntA.alltimesB+ ",image id:" + (Number(fntA.alltimesB)+3));
@@ -464,13 +481,15 @@ function fntRun(){
       fntA.requestId = window.requestAFrame(render);
       //set stop process
       if(fntA.shakeEng < 3){
-        fntA.moveA = fntA.moveA * 0.995;
+        fntA.moveA = fntA.moveA * 0.99;
+        if (fntA.moveA<0.8) {fntA.moveA=0.8};
       }
       //console.log("old: y0=" + y0 + ",y1=" + y1 + ",y2=" + y2 + ",move=" + move + ",fntA.alltimes=" + fntA.alltimes);
       //if(fntA.moveA<=4){
       fntA.allmoveA +=move; 
       fntA.allmoveB +=moveB; 
       fntA.shakeEng = fntA.shakeEng -1;
+      if (fntA.shakeEng<1) { fntA.shakeEng = 1};
       $(".playerinfoa .playrecord").html(fntA.allmoveA + '米');
       $(".playerinfob .playrecord").html(fntA.allmoveB + '米');
 
@@ -490,25 +509,34 @@ function fntRun(){
       }
       //Game AI
       if(fntA.gameLevel ===1 ){
-        if( (fntA.allmoveA - fntA.allmoveB) >300 ){
-          fntA.moveB = fntA.moveB * 1.12;
+        if( (fntA.allmoveA - fntA.allmoveB) >100 ){
+          fntA.moveB = fntA.moveB * 1.01;
         }
-        if( (fntA.allmoveB - fntA.allmoveA) >50 ){
-          fntA.moveB = fntA.moveB * 0.992;
+        if( (fntA.allmoveB - fntA.allmoveA) >350 ){
+          fntA.moveB = fntA.moveB * 0.9992;
         }
-        fntA.moveB = Math.min( 3, Math.max(1,fntA.moveB));
+        fntA.moveB = Math.min( 6, Math.max(1,fntA.moveB));
       }else if(fntA.gameLevel ===2){
-        if( (fntA.allmoveA - fntA.allmoveB) >200 ){
-          fntA.moveB = fntA.moveB * 1.22;
+        if( (fntA.allmoveA - fntA.allmoveB) >30 ){
+          fntA.moveB = fntA.moveB * 1.05;
         }
-        if( (fntA.allmoveB - fntA.allmoveA) >200 ){
-          fntA.moveB = fntA.moveB * 0.998;
+        if( (fntA.allmoveB - fntA.allmoveA) >800 ){
+          fntA.moveB = fntA.moveB * 0.9992;
         }
-        fntA.moveB = Math.min( 4, Math.max(2,fntA.moveB));
+        fntA.moveB = Math.min( 7, Math.max(4,fntA.moveB));
       }
-
+      var tmpPlayerTopa , tmpPlayerTopb;
+      tmpPlayerTopa = 320 - fntA.moveA*8;
+      tmpPlayerTopb = 320 - fntA.moveB*10;
+      $('.playera').css('top',tmpPlayerTopa+'px');
+      $('.playerb').css('top',tmpPlayerTopb+'px');
       //the hard game level
-
+      //mini map
+      var tmpMinia = 430 - (fntA.allmoveA * fntA.h/9600)
+      ,   tmpMinib = 430 - (fntA.allmoveB * fntA.h/9600);
+      //console.log('tmpMinib:' +tmpMinia+',tmpMinib:' + tmpMinia);
+      ctxMini.drawImage( fntA.imageMini,9,tmpMinia,29,28);
+      ctxMini.drawImage( fntA.imageMini,59,tmpMinib,29,28);
 
     }
     // handle multiple browsers for requestAnimationFrame()
@@ -537,21 +565,22 @@ function fntRun(){
         break;
       // shake event
       case fntA.key + "_changebg":
-        console.log('fntA.gameOn:' + fntA.gameOn +'fntA.shakerecord:' + fntA.shakerecord +'fntA.gameFinish:' + fntA.gameFinish );
-        if(!fntA.gameOn && fntA.shakerecord<5){
+        //console.log('fntA.gameOn:' + fntA.gameOn +'fntA.shakerecord:' + fntA.shakerecord +'fntA.gameFinish:' + fntA.gameFinish );
+        if(!fntA.gameOn && fntA.shakerecord<15){
           showSubMask('gamemask','connection');
           fntA.gameOn = true;
-        }else if(fntA.gameOn && fntA.shakerecord===5 && !fntA.gameFinish){
+        }else if(fntA.gameOn && fntA.shakerecord===15 && !fntA.gameFinish){
           showSubMask('gamemask','countdown');
           //console.log('call 1 countdown');
           countdownNewTime(3);
         }
         shakeEventDidOccur();
 
-        if(fntA.moveA<4){
-          fntA.moveA = fntA.moveA * 1.09;
+        if(fntA.moveA<7){
+          fntA.moveA = fntA.moveA * 1.16;
         }
-        fntA.shakeEng = fntA.shakeEng + 3 ;
+        fntA.shakeEng = fntA.shakeEng + 4 ;
+        
 
         break;
     }
@@ -567,9 +596,9 @@ function shakeEventDidOccur() {
 $(document).ready(function(){
 	fntA.key = NewGuid();
 	var pageUrl = window.location.href;
-	pageUrl=pageUrl.replace(/pc.html#\/run/g,"m.html");
-  pageUrl=pageUrl.replace(/pc.html#run/g,"m.html");
-  pageUrl=pageUrl.replace(/pc.html/g,"m.html");
+	pageUrl=pageUrl.replace(/index.html#\/run/g,"m.html");
+  pageUrl=pageUrl.replace(/index.html#run/g,"m.html");
+  pageUrl=pageUrl.replace(/index.html/g,"m.html");
 
 	fntA.gameLevel = 1;
 	fntA.shakerecord = 0;
